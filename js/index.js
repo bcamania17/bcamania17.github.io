905c192ba0758f56abe4f3a0696536b50a7f8fe0
Chirayu Jain <jain.chirayu98@gmail.com>
2017-01-09 10:04:10 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ var graph = new HorizontalBarGraph('#my-graph', [
   {label: "hermanos de sangre ",         inner_label: "100",   value: 100,  color: "#2980b9" },
   {label: "20.32 centimeters",           inner_label: "50",   value: 50,  color: "#95a5a6" },
   {label: "Jaesus and His Disciples",    inner_label: "0",   value: 0,  color: "#16a085" },
-  {label: "J.E.L.A.N.I.E.",              inner_label: "1100",   value: 1100,  color: "#e74c3c" },
+  {label: "J.E.L.A.N.I.E.",              inner_label: "1150",   value: 1150,  color: "#e74c3c" },
   {label: "BEARS ON FIRE",               inner_label: "0",   value: 0,  color: "#e67e22" },
   {label: "SIX012",                      inner_label: "0",   value: 0,  color: "#27ae60" },
   {label: "Senioritas",                  inner_label: "0",   value: 0,  color: "#2980b9" },
