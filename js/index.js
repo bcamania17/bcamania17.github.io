ef7eaacac131bc434c814927572f14fe8b6d848d
Chirayu Jain <jain.chirayu98@gmail.com>
2017-02-02 10:14:18 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ var graph = new HorizontalBarGraph('#my-graph', [
   {label: "Business Girls",              inner_label: "0",   value: 0,  color: "#2980b9" },
   {label: "#WeJustWantToGraduate",       inner_label: "0",   value: 0,  color: "#95a5a6" },
   {label: "Wildcats",                    inner_label: "1000",   value: 1000,  color: "#16a085" },
-  {label: "Fyfers",                      inner_label: "350",   value: 350,  color: "#e74c3c" },
+  {label: "Fyfers",                      inner_label: "2150",   value: 2150,  color: "#e74c3c" },
   {label: "Phineas and Ferb",            inner_label: "5750",   value: 5750,  color: "#e67e22" },
   {label: "Detal Man",                   inner_label: "0",   value: 0,  color: "#27ae60" },
   {label: "hermanos de sangre ",         inner_label: "300",   value: 300,  color: "#2980b9" },
