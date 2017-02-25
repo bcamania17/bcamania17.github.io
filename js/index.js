66e7bad72bb7156f7a4fa5e7ff195d74a44826a1
Shaantam Chawla <shaan.c99@gmail.com>
2017-02-25 18:30:13 -0500

    Creamsicles +50 (kissing a statue)

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ HorizontalBarGraph.prototype.draw = function() {
 };
 
 var graph = new HorizontalBarGraph('#my-graph', [
-  {label: "Creamsicles",                 inner_label: "3550",   value: 3550,  color: "#e74c3c" },
+  {label: "Creamsicles",                 inner_label: "3600",   value: 3600,  color: "#e74c3c" },
   {label: "Mandem",                      inner_label: "100",   value: 100,  color: "#e67e22" },
   {label: "brazy yeeks",                 inner_label: "0",   value: 0,  color: "#27ae60" },
   {label: "Baseball Academy",            inner_label: "0",   value: 0,  color: "#2980b9" },
